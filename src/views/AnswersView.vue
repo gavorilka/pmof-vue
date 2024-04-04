4f69ada3b14afd44d44357cbbe1657699d55ed7e
gavorilka <59140477+gavorilka@users.noreply.github.com>
2024-04-04 13:00:00 +0300

    lazy load for ответы на вопросы

--- src/views/AnswersView.vue
+++ src/views/AnswersView.vue
@@ -50,6 +50,7 @@
               <iframe
                   :src="accItem.src"
                   :title="accItem.title"
+                  loading="lazy"
                   frameborder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                   referrerpolicy="strict-origin-when-cross-origin"
